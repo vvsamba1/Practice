bd9c032bd4058cd9dfba3f32ce0624da0ad7f851
vvsamba1 <165955292+vvsamba1@users.noreply.github.com>
2024-04-03 16:13:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Given array
+        int[] given = { 1, 1, 2, 2, 2, 3, 3, 3, 3, 5, 5, 6, 7, 8 };
+
+        Dictionary<int,int> arrayCount = new Dictionary<int, int>();
+
+        //If given array is empty
+
+        for(int i = 0; i < given.Length; i++){
+            if (arrayCount.ContainsKey(given[i])){
+                arrayCount[given[i]]++;
+            }
+            else{
+                arrayCount.Add(given[i],1);
+            }
+        }
+
+        if(arrayCount.Count == 0){
+            Console.WriteLine("The given array is empty!");
+        }
+        else{
+            foreach (var item in arrayCount)
+            {
+                Console.Write(item.Key + " = " + item.Value + ", ");
+            }
+        }
+
+    }
+}
